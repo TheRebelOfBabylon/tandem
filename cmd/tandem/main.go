a93c0e7d9dde4ad2839cdee9078c72c23175986d
TheRebelOfBabylon <therebelofbabylon@protonmail.com>
2022-11-19 23:11:10 -0500

    using Laniakea signal interceptor instead so signal intercept logs are captured in our logs

--- cmd/tandem/main.go
+++ cmd/tandem/main.go
@@ -4,7 +4,9 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"time"
 
+	"github.com/SSSOC-CAN/laniakea/intercept"
 	"github.com/SSSOC-CAN/laniakea/utils"
 	"github.com/TheRebelOfBabylon/tandem/config"
 	"github.com/TheRebelOfBabylon/tandem/logger"
@@ -19,6 +21,11 @@ func main() {
 		fmt.Fprintf(os.Stdout, "tandem version %s\n", u.AppVersion)
 		os.Exit(0)
 	}
+	interceptor, err := intercept.InitInterceptor()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Could not initialize signal interceptor: %v\n", err)
+		os.Exit(1)
+	}
 	cfg, err := config.InitConfig(*configDir)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Could not initialize configuration: %v\n", err)
@@ -29,10 +36,16 @@ func main() {
 		fmt.Fprintf(os.Stderr, "Could not initialize logger: %v\n", err)
 		os.Exit(1)
 	}
+	interceptor.Logger = &log
 	log.Info().Msg("testing info")
 	log.Debug().Msg("testing debug")
 	log.Warn().Msg("testing warn")
 	log.Error().Msg("testing error")
 	log.Trace().Msg("testing trace")
-	log.Fatal().Msg("testing fatal")
+	//log.Fatal().Msg("testing fatal")
+	go func() {
+		time.Sleep(5 * time.Second)
+		interceptor.RequestShutdown()
+	}()
+	<-interceptor.ShutdownChannel()
 }
