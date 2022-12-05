f55700ac1ea3f47faf9982b39d777666d73c8029
TheRebelOfBabylon <therebelofbabylon@protonmail.com>
2022-12-04 19:30:49 -0500

    created new mongodb connection factory function

--- db/mongodb.go
+++ db/mongodb.go
@@ -1,12 +1,25 @@
 package db
 
 import (
-	"sync"
+	"context"
+	"fmt"
 
+	"github.com/TheRebelOfBabylon/tandem/config"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type MongoDB struct {
 	client *mongo.Client
-	sync.RWMutex
+}
+
+// NewDBConnection creates a connection with
+func NewDBConnection(ctx context.Context, cfg *config.Data) (*MongoDB, error) {
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(fmt.Sprintf("mongodb://%s:%s@%s/?maxPoolSize=20&w=majority", cfg.DBUser, cfg.DBPass, cfg.DBAddr)))
+	if err != nil {
+		return nil, err
+	}
+	return &MongoDB{
+		client: client,
+	}, nil
 }
