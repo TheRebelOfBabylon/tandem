4214095ed5993e45453271114edda287690db4f1
TheRebelOfBabylon <therebelofbabylon@protonmail.com>
2022-12-08 10:27:54 -0500

    added some extra TODO comments

--- tandem.go
+++ tandem.go
@@ -30,10 +30,8 @@ var (
 func loggingHandler(next http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request, log zerolog.Logger, dbClient *db.MongoDB, p *nostr.SafeParser) {
 		o := &ResponseObserver{ResponseWriter: w}
-		t1 := time.Now()
 		next.ServeHTTP(o, r)
-		t2 := time.Now()
-		log.Info().Msgf("[%s] %q %d %v", r.Method, r.URL.String(), o.status, t2.Sub(t1))
+		log.Info().Msgf("[%s] %q %d", r.Method, r.URL.String(), o.status)
 	}
 	ch, _ := next.(*CustomHandler)
 	return &CustomHandler{Logger: ch.Logger, handlerFunc: fn, Client: ch.Client, Parser: ch.Parser}
@@ -51,6 +49,7 @@ func newConnectionHandler(w http.ResponseWriter, r *http.Request, log zerolog.Lo
 	log.Debug().Msgf("Client IP: %v", conn.RemoteAddr().String())
 loop:
 	for {
+		// TODO - Add rate limiting
 		messageType, message, err := conn.ReadMessage()
 		if err != nil {
 			log.Error().Msgf("Error during message reading: %v", err)
@@ -66,7 +65,8 @@ loop:
 				// TODO - Send error message as per NIP-20
 				continue
 			}
-			// if no errors, store and check if active filters require sending it down the pipeline
+			// Store and check if active filters require sending it down the pipeline
+			// Check if event has already been saved in db and send error for duplicates
 			log.Debug().Msgf("Parsed Nostr message: %v", msg)
 		case websocket.CloseMessage:
 			log.Debug().Msgf("closing connection to %v...", conn.RemoteAddr().String())
@@ -120,10 +120,6 @@ func Main(cfg *config.Config, log zerolog.Logger, interceptor *intercept.Interce
 			log.Error().Msgf("Unexpected error when stopping HTTP server: %v", err)
 		}
 	}()
-
-	// Run the Nostr protocol overtop the WebSocket connections
-	// Listen for key Nostr events
-	// Take appropriate actions
 	<-interceptor.ShutdownChannel()
 	// Graceful shutdown of HTTP server
 	httpSrv.Shutdown(ctx)
