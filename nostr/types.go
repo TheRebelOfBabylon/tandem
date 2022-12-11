640fff9b67f5a6717e65ee03dc41f8bcc4c474ff
TheRebelOfBabylon <therebelofbabylon@protonmail.com>
2022-12-10 22:03:52 -0500

    created Event struct methods to convert to BSON format

--- nostr/types.go
+++ nostr/types.go
@@ -3,6 +3,8 @@ package nostr
 import (
 	"crypto/sha256"
 	"fmt"
+
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 type Event struct {
@@ -39,6 +41,32 @@ func (e *Event) CreateEventId() []byte {
 	serial := fmt.Sprintf("[0,\"%s\",%v,%v,%s,\"%s\"]", e.Pubkey, e.CreatedAt, e.Kind, serialTags, e.Content)
 	hash := sha256.Sum256([]byte(serial))
 	return hash[:]
+}
+
+// ToBson translates the Event into bson format for storage in MongoDB
+func (e *Event) ToBson() bson.D {
+	var bTags bson.A
+	for _, tag := range e.Tags {
+		var bTag bson.A
+		for _, t := range tag {
+			bTag = append(bTag, t)
+		}
+		bTags = append(bTags, bTag)
+	}
+	return bson.D{
+		{"event_id", e.EventId},
+		{"pubkey", e.Pubkey},
+		{"created_at", e.CreatedAt},
+		{"kind", e.Kind},
+		{"tags", bTags},
+		{"content", e.Content},
+		{"sig", e.Sig},
+	}
+}
+
+// EventIdBsonFilter returns a BSON object for a filter which filters by event id
+func (e *Event) EventIdBsonFilter() bson.D {
+	return bson.D{{"event_id", e.EventId}}
 }
 
 type Filter struct {
