053d3f8a167f584b4525fcbc4dfd50de5586ef5d
TheRebelOfBabylon <therebelofbabylon@protonmail.com>
2022-12-10 22:03:10 -0500

    InsertEvent method created for custom db to abstract MongoDB api specifics

--- db/mongodb.go
+++ db/mongodb.go
@@ -4,9 +4,15 @@ import (
 	"context"
 	"fmt"
 
+	bg "github.com/SSSOCPaulCote/blunderguard"
 	"github.com/TheRebelOfBabylon/tandem/config"
+	"github.com/TheRebelOfBabylon/tandem/nostr"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const (
+	ErrEventDuplicate = bg.Error("duplicate event")
 )
 
 type MongoDB struct {
@@ -22,4 +28,15 @@ func NewDBConnection(ctx context.Context, cfg *config.Data) (*MongoDB, error) {
 	return &MongoDB{
 		client: client,
 	}, nil
+}
+
+//InsertEvent inserts a new event into the database
+func (db *MongoDB) InsertEvent(ctx context.Context, event nostr.Event) error {
+	// Check if id is already in database
+	result := db.client.Database("nostr").Collection("event").FindOne(ctx, event.EventIdBsonFilter())
+	if result.Err() == nil {
+		return ErrEventDuplicate
+	}
+	_, err := db.client.Database("nostr").Collection("event").InsertOne(ctx, event.ToBson())
+	return err
 }
